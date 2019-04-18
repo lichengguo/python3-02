579d6b3dcdb6cc238738ffb3dd3418d5b2757427
alnk <1029612787@qq.com>
2019-04-18 12:24:14 +0800

    day12 老师笔记和笔记整理

---
+++ 12 day12/01 老师笔记/05 异常.py
@@ -0,0 +1,69 @@
+
+
+# try:
+#     print(2 + 5)
+#     print(yuan)
+#     print("ok")
+# except Exception as e:
+#     print("错误信息：",str(e))
+#
+# finally:
+#     print("OVER")
+
+###############################
+
+
+# while 1:
+#     try:
+#         x = int(input(">>>"))
+#         y = int(input(">>>"))
+#         print(x+y)
+#     except Exception as e:
+#         print("非法输入",str(e))
+
+
+# s="hello"
+#
+# s2=str("world")
+# print(s2)
+
+# def foo(x,y):
+#     print(x+y)
+#
+# bar=lambda x,y:print(x+y)
+# bar(2,3)
+#
+# (lambda x,y:print(x+y))(3,6)
+
+
+
+
+##############
+# LEGB
+x=100
+def  foo():
+    x=20
+    print(x)
+    def abc():
+        print(x)
+
+def bar():
+    print(x)
+
+
+
+
+
+
+for  i in [123,2344,45,456,567]:
+    print("OK")
+
+print(i)
+
+
+
+
+
+
+
+
